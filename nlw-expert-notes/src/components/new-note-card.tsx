2fa5cf1f088f20b23f8ce988492ce42c16dea24b
Igor Viana <contato.igorvianadasilva@gmail.com>
2024-02-07 14:39:12 -0300

    Compenentização dos Cards, Criação dos Modals com a biblioteca RadixUI, e uso dos primeiros eventsHandlers

--- nlw-expert-notes/src/components/new-note-card.tsx
+++ nlw-expert-notes/src/components/new-note-card.tsx
@@ -1,12 +1,90 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from 'sonner'
+
 export function NewNoteCard() {
-    return (
-        <div className='rounded-md bg-slate-700 p-5 space-y-3'>
-          <span className='text-small font-medium text-slate-200'>
-            Adicionar nota
-          </span>
-          <p className='text-sm leading-6 text-slate-400' >
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-          </p>
-        </div>
-    )
+  const [shouldShowOnboarding, setshouldShowOnboarding] = useState(true);
+  const [content, setContent] = useState('')
+
+  function handleStartEditor() {
+    setshouldShowOnboarding(false);
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    //Se o valor da TextArea for apagado, o mesmo será fechado e as opções voltarão a aparecer
+    setContent(event.target.value)
+
+    if (event.target.value === '') {
+      setshouldShowOnboarding(true)
+    }
+  }
+
+  function handleSaveNote(event: FormEvent) {
+    event.preventDefault()
+    console.log(content)
+    toast.success('Nota criada com sucesso')
+  }
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 p-5 gap-3 text-left outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-small font-medium text-slate-200">
+          Adicionar nota
+        </span>
+        <p className="text-sm leading-6 text-slate-400">
+          Grave uma nota em áudio que será convertida para texto
+          automaticamente.
+        </p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50">
+          <Dialog.Content className="fixed -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none overflow-hidden">
+            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p1.5 text-slate-400 hover:text-slate-100">
+              <X className="size-5" />
+            </Dialog.Close>
+
+            <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+              <div className="flex flex-1 flex-col gap-3 p-5">
+                <span className="text-small font-medium text-slate-300">
+                  Adicionar nota
+                </span>
+                {shouldShowOnboarding ? (
+                  <p className="text-sm leading-6 text-slate-400">
+                    Comece{" "}
+                    <button className="font-medium text-lime-400 hover:underline">
+                      gravando uma nota
+                    </button>{" "}
+                    em áudio ou se preferir{" "}
+                    <button
+                      onClick={handleStartEditor}
+                      className="font-medium text-lime-400 hover:underline"
+                    >
+                      utilize apenas texto.
+                    </button>
+                  </p>
+                ) : (
+                  <textarea
+                    autoFocus
+                    className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none" onChange={handleContentChanged}
+                  ></textarea>
+                )}
+              </div>
+
+              <button
+                type="submit"
+                className="w-full bg-lime-400 py-4 text-center text-sm text-slate-300 outline-none front-medium hover:bg-lime-500"
+              >
+                <span className="text-lime-950 group-hover:underline">
+                  Salvar nota
+                </span>
+              </button>
+            </form>
+            
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
 }
